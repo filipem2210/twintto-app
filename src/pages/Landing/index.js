d9e26f379a803fad5dd09a5b2ddb1ad2d1e3149d
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-14 20:51:30 -0300

    add landing page

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import logo from '../../assets/logo.png';
+
+import {
+  Container,
+  Logo,
+  Wrapper,
+  Title,
+  SignUpButton,
+  SignUpButtonText,
+  SignInWrapper,
+  SignInText,
+  SignInButton,
+  SignInButtonText,
+} from './styles';
+
+export default function LandingScreen() {
+  return (
+    <Container>
+      <Logo source={logo} resizeMode="contain" />
+      <Wrapper>
+        <Title>Veja o que está acontencendo no mundo neste momento.</Title>
+        <SignUpButton>
+          <SignUpButtonText>Criar Conta</SignUpButtonText>
+        </SignUpButton>
+      </Wrapper>
+      <SignInWrapper>
+        <SignInText>Já tem uma conta? </SignInText>
+        <SignInButton>
+          <SignInButtonText>Entrar</SignInButtonText>
+        </SignInButton>
+      </SignInWrapper>
+    </Container>
+  );
+}
---
+++ src/pages/Landing/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 30px 30px 30px;
+`;
+
+export const Logo = styled.Image``;
+
+export const Wrapper = styled.View`
+  flex: 1;
+  width: 100%;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  max-width: 70%;
+  line-height: 40px;
+`;
+
+export const SignUpButton = styled(RectButton)`
+  width: 100%;
+  background-color: #33a1f2;
+  border-radius: 50px;
+  padding: 6px;
+  margin-top: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignUpButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 23px;
+`;
+
+export const SignInWrapper = styled.View`
+  width: 100%;
+  flex-direction: row;
+`;
+
+export const SignInText = styled.Text`
+  color: #8899a6;
+`;
+
+export const SignInButton = styled.TouchableOpacity``;
+
+export const SignInButtonText = styled.Text`
+  color: #33a1f2;
+`;
