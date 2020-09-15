3593d5c4c78729601d9cd443740b64536af01317
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-14 23:48:46 -0300

    add signup inputs, formik and yup validation

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,152 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Formik, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-import {Container} from './styles';
+import logo from '../../assets/logo.png';
 
-export default function SignUp() {
+import {
+  Container,
+  TopMenuWrapper,
+  BackButton,
+  Logo,
+  Title,
+  FormWrapper,
+  Input,
+  ErrorWrapper,
+  SignUpButton,
+  SignUpButtonText,
+} from './styles';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('O nome completo é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha precisa ter no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUpScreen({navigation: {goBack}}) {
+  const [nameInputFocused, setNameInputFocused] = useState(false);
+  const [emailInputFocused, setEmailInputFocused] = useState(false);
+  const [passwordInputFocused, setPasswordInputFocused] = useState(false);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSignUp(data) {
+    console.log(data);
+  }
+
   return (
     <Container>
-      <Text>SignUp</Text>
+      <TopMenuWrapper>
+        <BackButton onPress={() => goBack()}>
+          <Icon name="arrowleft" size={25} color="#33a1f2" />
+        </BackButton>
+        <Logo source={logo} resizeMode="contain" />
+      </TopMenuWrapper>
+      <Title>Criar sua conta</Title>
+      <FormWrapper>
+        <Formik
+          initialValues={{name: '', email: '', password: ''}}
+          validationSchema={validationSchema}
+          onSubmit={(values) => handleSignUp(values)}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            errors,
+            isSubmitting,
+            values,
+          }) => (
+            <>
+              <Input
+                placeholder="Nome"
+                value={values.name}
+                onChangeText={handleChange('name')}
+                onBlur={() => {
+                  handleBlur('name');
+                  setNameInputFocused(false);
+                }}
+                autoCompleteType="name"
+                autoCorrect={false}
+                autoCapitalize="words"
+                returnKeyType="next"
+                autoFocus
+                onFocus={() => {
+                  setNameInputFocused(true);
+                }}
+                underlineColorAndroid={nameInputFocused ? '#33a1f2' : '#8899a6'}
+                selectionColor="#33a1f2"
+                onSubmitEditing={() => emailRef.current.focus()}
+              />
+              <ErrorWrapper>
+                <ErrorMessage name="name" />
+              </ErrorWrapper>
+
+              <Input
+                placeholder="E-mail"
+                value={values.email}
+                onChangeText={handleChange('email')}
+                onBlur={() => {
+                  handleBlur('email');
+                  setEmailInputFocused(false);
+                }}
+                keyboardType="email-address"
+                autoCompleteType="email"
+                autoCorrect={false}
+                autoCapitalize="none"
+                ref={emailRef}
+                returnKeyType="next"
+                onFocus={() => {
+                  setEmailInputFocused(true);
+                }}
+                underlineColorAndroid={
+                  emailInputFocused ? '#33a1f2' : '#8899a6'
+                }
+                selectionColor="#33a1f2"
+                onSubmitEditing={() => passwordRef.current.focus()}
+              />
+              <ErrorWrapper>
+                <ErrorMessage name="email" />
+              </ErrorWrapper>
+
+              <Input
+                secureTextEntry
+                placeholder="Senha"
+                value={values.password}
+                onChangeText={handleChange('password')}
+                onBlur={() => {
+                  handleBlur('password');
+                  setPasswordInputFocused(false);
+                }}
+                autoCompleteType="off"
+                maxLength={50}
+                ref={passwordRef}
+                returnKeyType="send"
+                onFocus={() => {
+                  setPasswordInputFocused(true);
+                }}
+                underlineColorAndroid={
+                  passwordInputFocused ? '#33a1f2' : '#8899a6'
+                }
+                selectionColor="#33a1f2"
+                onSubmitEditing={handleSubmit}
+              />
+              <ErrorWrapper>
+                <ErrorMessage name="password" />
+              </ErrorWrapper>
+            </>
+          )}
+        </Formik>
+      </FormWrapper>
+
+      <SignUpButton>
+        <SignUpButtonText>Inscrever-se</SignUpButtonText>
+      </SignUpButton>
     </Container>
   );
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,55 @@
+import {Platform} from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+  behavior: Platform.OS === 'ios' ? 'padding' : 'height',
+})`
   flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 30px;
+`;
+
+export const TopMenuWrapper = styled.View`
+  flex-direction: row;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const Logo = styled.Image`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  width: 100%;
+  margin-top: 30px;
+`;
+
+export const FormWrapper = styled.View`
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const Input = styled.TextInput`
+  font-size: 19px;
+`;
+
+export const ErrorWrapper = styled.Text`
+  color: #c0392b;
+`;
+
+export const SignUpButton = styled.TouchableOpacity`
+  background-color: #33a1f2;
+  border-radius: 50px;
+  padding: 10px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignUpButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
 `;
