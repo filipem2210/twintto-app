0b8dae9c6861a81c2fc5b7eac6b7516aa52718f6
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-16 13:48:27 -0300

    add drawer items to app routes

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
+import DrawerItems from './drawerItems';
 import HomeTabs from './bottomTabs';
 
 const AppDrawer = createDrawerNavigator();
 
 export default function AppRoutes() {
   return (
-    <AppDrawer.Navigator initialRouteName="Home" drawerStyle={{width: '90%'}}>
+    <AppDrawer.Navigator
+      initialRouteName="Home"
+      drawerStyle={{width: '90%'}}
+      drawerContent={(props) => <DrawerItems {...props} />}>
       <AppDrawer.Screen name="Home" component={HomeTabs} />
     </AppDrawer.Navigator>
   );
