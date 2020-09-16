6a8cf0501c60ea0afcd1b7594fdda8fabdb2cdbc
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-15 21:52:53 -0300

    add signin page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,159 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Formik, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-import {Container} from './styles';
+import logo from '../../assets/logo.png';
 
-export default function SignInScreen() {
+import {
+  Container,
+  TopMenuWrapper,
+  BackButton,
+  Logo,
+  SignUpButton,
+  SignUpButtonText,
+  Title,
+  FormWrapper,
+  InputWrapper,
+  Input,
+  PasswordVisibilityButton,
+  PasswordVisibilityButtonIcon,
+  ErrorWrapper,
+  SignInButtonWrapper,
+  SignInButton,
+  SignInButtonText,
+  ForgotPasswordButton,
+  ForgotPasswordButtonText,
+} from './styles';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha precisa ter no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignInScreen({navigation}) {
+  const [emailInputFocused, setEmailInputFocused] = useState(false);
+  const [passwordInputFocused, setPasswordInputFocused] = useState(false);
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
+  const passwordRef = useRef();
+
+  function handleSignIn(data) {
+    console.log(data);
+  }
+
   return (
     <Container>
-      <Text>SignIn</Text>
+      <TopMenuWrapper>
+        <BackButton onPress={() => navigation.goBack()}>
+          <Icon name="arrowleft" size={25} color="#33a1f2" />
+        </BackButton>
+        <Logo source={logo} resizeMode="contain" />
+        <SignUpButton onPress={() => navigation.navigate('SignUp')}>
+          <SignUpButtonText>Inscreva-se</SignUpButtonText>
+        </SignUpButton>
+      </TopMenuWrapper>
+      <Title>Entrar no Twitter</Title>
+      <FormWrapper>
+        <Formik
+          initialValues={{email: '', password: ''}}
+          validationSchema={validationSchema}
+          onSubmit={(values) => handleSignIn(values)}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            errors,
+            isSubmitting,
+            values,
+          }) => (
+            <>
+              <Input
+                placeholder="E-mail"
+                value={values.email}
+                onChangeText={handleChange('email')}
+                onBlur={() => {
+                  handleBlur('email');
+                  setEmailInputFocused(false);
+                }}
+                keyboardType="email-address"
+                autoCompleteType="email"
+                autoCorrect={false}
+                autoCapitalize="none"
+                autoFocus
+                returnKeyType="next"
+                onFocus={() => {
+                  setEmailInputFocused(true);
+                }}
+                underlineColorAndroid={
+                  emailInputFocused ? '#33a1f2' : '#8899a6'
+                }
+                selectionColor="#33a1f2"
+                onSubmitEditing={() => passwordRef.current.focus()}
+              />
+              <ErrorWrapper>
+                <ErrorMessage name="email" />
+              </ErrorWrapper>
+
+              <InputWrapper>
+                <Input
+                  secureTextEntry={passwordHidden}
+                  placeholder="Senha"
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  onBlur={() => {
+                    handleBlur('password');
+                    setPasswordInputFocused(false);
+                  }}
+                  autoCapitalize="none"
+                  autoCompleteType="off"
+                  autoCorrect={false}
+                  maxLength={50}
+                  ref={passwordRef}
+                  returnKeyType="done"
+                  onFocus={() => {
+                    setPasswordInputFocused(true);
+                  }}
+                  underlineColorAndroid={
+                    passwordInputFocused ? '#33a1f2' : '#8899a6'
+                  }
+                  selectionColor="#33a1f2"
+                  onSubmitEditing={handleSubmit}
+                />
+                <PasswordVisibilityButton
+                  onPress={() => setPasswordHidden((prevValue) => !prevValue)}>
+                  <PasswordVisibilityButtonIcon
+                    name="eye"
+                    size={20}
+                    color={passwordHidden ? '#8899a6' : '#33a1f2'}
+                    style={!passwordHidden && {opacity: 1}}
+                    clas
+                  />
+                </PasswordVisibilityButton>
+              </InputWrapper>
+              <ErrorWrapper>
+                <ErrorMessage name="password" />
+              </ErrorWrapper>
+
+              <ForgotPasswordButton>
+                <ForgotPasswordButtonText>
+                  Esqueceu sua senha?
+                </ForgotPasswordButtonText>
+              </ForgotPasswordButton>
+            </>
+          )}
+        </Formik>
+      </FormWrapper>
+
+      <SignInButtonWrapper>
+        <SignInButton>
+          <SignInButtonText>Entrar</SignInButtonText>
+        </SignInButton>
+      </SignInButtonWrapper>
     </Container>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,8 +1,104 @@
 import {Platform} from 'react-native';
 import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export const Container = styled.KeyboardAvoidingView.attrs({
   behavior: Platform.OS === 'ios' ? 'padding' : 'height',
 })`
   flex: 1;
+  align-items: center;
+  padding: 15px 0 0 0;
+  background-color: #fff;
+`;
+
+export const TopMenuWrapper = styled.View`
+  flex-direction: row;
+  padding: 0 10px;
+`;
+
+export const BackButton = styled.TouchableHighlight.attrs({
+  underlayColor: 'rgba(29, 161, 242, 0.1)',
+})`
+  padding: 5px;
+  border-radius: 50px;
+`;
+
+export const Logo = styled.Image`
+  flex: 1;
+`;
+
+export const SignUpButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.5,
+})``;
+
+export const SignUpButtonText = styled.Text`
+  color: #33a1f2;
+  font-weight: bold;
+`;
+
+export const Title = styled.Text`
+  width: 100%;
+  font-size: 23px;
+  font-weight: bold;
+  margin-top: 10px;
+  padding: 0 10px;
+`;
+
+export const FormWrapper = styled.View`
+  flex: 1;
+  width: 100%;
+  margin-top: 20px;
+  padding: 0 10px;
+`;
+
+export const InputWrapper = styled.View`
+  position: relative;
+`;
+
+export const Input = styled.TextInput`
+  font-size: 19px;
+`;
+
+export const PasswordVisibilityButton = styled.TouchableWithoutFeedback``;
+
+export const PasswordVisibilityButtonIcon = styled(Icon)`
+  position: absolute;
+  right: 5px;
+  top: 15px;
+  opacity: 0.4;
+`;
+
+export const ErrorWrapper = styled.Text`
+  color: #c0392b;
+`;
+
+export const ForgotPasswordButton = styled.TouchableHighlight`
+  padding: 5px;
+  align-items: center;
+`;
+
+export const ForgotPasswordButtonText = styled.Text`
+  color: #747d8c;
+`;
+
+export const SignInButtonWrapper = styled.View`
+  width: 100%;
+  align-items: flex-end;
+  padding: 10px;
+  border-top-width: 1px;
+  border-top-color: #bdc3c7;
+`;
+
+export const SignInButton = styled(RectButton)`
+  background-color: #33a1f2;
+  border-radius: 50px;
+  padding: 6px 16px;
+`;
+
+export const SignInButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
 `;
