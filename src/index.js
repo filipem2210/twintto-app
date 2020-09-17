4ae366503c32b0159ec23795fcb8afb068688d0c
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-16 21:04:48 -0300

    add dark/light theme support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState, useMemo} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+
+import {DarkTheme} from './styles/dark';
+import {LightTheme} from './styles/light';
+
+import {ThemeContext} from './contexts/ThemeContext';
 
 import Routes from './routes';
 
 export default function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const theme = isDarkTheme ? DarkTheme : LightTheme;
+
+  const themeContext = useMemo(
+    () => ({
+      toggleTheme: () => {
+        setIsDarkTheme((prevValue) => !prevValue);
+      },
+    }),
+    [],
+  );
+
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <Routes />
+    <NavigationContainer theme={theme}>
+      <ThemeContext.Provider value={themeContext}>
+        <StatusBar barStyle={isDarkTheme ? 'dark-content' : 'light-content'} />
+        <Routes />
+      </ThemeContext.Provider>
     </NavigationContainer>
   );
 }
