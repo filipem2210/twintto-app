b8c81493e61dfdea6d94ecf0eefeb582e7ae2f28
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-17 20:47:06 -0300

    tweet component

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -1,15 +1,127 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {useTheme} from '@react-navigation/native';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import IoIcon from 'react-native-vector-icons/Ionicons';
 
-import {Container} from './styles';
+import {
+  Container,
+  Retweeted,
+  Body,
+  Avatar,
+  Content,
+  Header,
+  Name,
+  User,
+  Date,
+  Description,
+  ImageWrapper,
+  ImageContent,
+  Icons,
+  Comments,
+  CommentsNumber,
+  Retweets,
+  RetweetsNumber,
+  Likes,
+  LikesNumber,
+  Actions,
+} from './styles';
 
-export default function Tweet() {
+const iconSize = 25;
+
+export default function Tweet({
+  avatar,
+  name,
+  user,
+  date,
+  description,
+  image,
+  comments,
+  retweets,
+  likes,
+  retweet,
+}) {
   const {colors} = useTheme();
 
   return (
     <Container style={{borderBottomColor: colors.border}}>
-      <Text style={{color: colors.text}}>Tweet</Text>
+      {retweet && (
+        <Retweeted>
+          <MCIcon name="twitter-retweet" size={iconSize} color={colors.gray} />
+          <Text style={{color: colors.gray}}>Você retweetou</Text>
+        </Retweeted>
+      )}
+      <Body>
+        <Avatar source={{uri: avatar}} />
+
+        <Content>
+          <Header>
+            <Name style={{color: colors.text}}>{name} </Name>
+            <User style={{color: colors.gray}}>{user}</User>
+            <Text style={{color: colors.gray}}> . </Text>
+            <Date style={{color: colors.gray}}>{date}</Date>
+          </Header>
+
+          <Description>
+            <Text style={{color: colors.text}}>{description}</Text>
+          </Description>
+
+          {image && (
+            <ImageWrapper style={{borderColor: colors.border}}>
+              <ImageContent
+                source={{uri: image}}
+                style={{borderColor: colors.border}}
+              />
+            </ImageWrapper>
+          )}
+
+          <Icons>
+            <Comments>
+              <IoIcon
+                name="chatbubble-outline"
+                size={iconSize}
+                color={comments > 0 ? colors.twitter : colors.gray}
+              />
+              <CommentsNumber
+                style={{color: comments > 0 ? colors.twitter : colors.gray}}>
+                {' '}
+                {comments}
+              </CommentsNumber>
+            </Comments>
+            <Retweets>
+              <MCIcon
+                name="twitter-retweet"
+                size={30}
+                color={retweets > 0 ? colors.retweet : colors.gray}
+              />
+              <RetweetsNumber
+                style={{color: retweets > 0 ? colors.retweet : colors.gray}}>
+                {' '}
+                {retweets}
+              </RetweetsNumber>
+            </Retweets>
+            <Likes>
+              <IoIcon
+                name="heart-outline"
+                size={iconSize}
+                color={likes > 0 ? colors.like : colors.gray}
+              />
+              <LikesNumber
+                style={{color: likes > 0 ? colors.like : colors.gray}}>
+                {' '}
+                {likes}
+              </LikesNumber>
+            </Likes>
+            <Actions>
+              <IoIcon
+                name="share-social-outline"
+                size={iconSize}
+                color={colors.gray}
+              />
+            </Actions>
+          </Icons>
+        </Content>
+      </Body>
     </Container>
   );
 }
--- src/components/Tweet/styles.js
+++ src/components/Tweet/styles.js
@@ -1,6 +1,112 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
 
 export const Container = styled.View`
+  padding: 14px 16px;
   border-bottom-width: 1px;
-  padding: 15px;
+`;
+
+export const Retweeted = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-left: 30px;
+`;
+
+export const Body = styled.View`
+  margin-top: 3px;
+  position: relative;
+`;
+
+export const Avatar = styled.Image`
+  width: 49px;
+  height: 49px;
+  border-radius: 49px;
+  flex-shrink: 0;
+  background-color: gray;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  margin-top: 2px;
+  padding-left: 59px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+`;
+
+export const Name = styled.Text`
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const User = styled.Text`
+  font-size: 15px;
+`;
+
+export const Date = styled.Text`
+  font-size: 15px;
+`;
+
+export const Description = styled.Text`
+  font-size: 15px;
+  margin-top: 4px;
+`;
+
+export const ImageWrapper = styled.View`
+  margin-top: 12px;
+  border-width: 1px;
+  border-radius: 15px;
+`;
+
+export const ImageContent = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
+  width: 100%;
+  height: 150px;
+`;
+
+export const Icons = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 11px;
+  width: 100%;
+`;
+
+const bottomIconsCSS = css`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Comments = styled.View`
+  ${bottomIconsCSS}
+`;
+
+export const CommentsNumber = styled.Text`
+  font-size: 14px;
+`;
+
+export const Retweets = styled.View`
+  ${bottomIconsCSS}
+`;
+
+export const RetweetsNumber = styled.Text`
+  font-size: 14px;
+`;
+
+export const Likes = styled.View`
+  ${bottomIconsCSS}
+`;
+
+export const LikesNumber = styled.Text`
+  font-size: 14px;
+`;
+
+export const Actions = styled.View`
+  ${bottomIconsCSS}
 `;
