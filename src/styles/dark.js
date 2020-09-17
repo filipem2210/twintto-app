b8ae29bc2aa5886179a0fcae0f573d55fbb6d33a
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-16 21:02:16 -0300

    add dark theme

---
+++ src/styles/dark.js
@@ -0,0 +1,13 @@
+export const DarkTheme = {
+  dark: true,
+  colors: {
+    twitter: '#33a1f2',
+    primary: 'rgb(255, 45, 85)',
+    background: '#000000',
+    card: '#000000',
+    text: '#ffffff',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+    inactiveTintColor: '#8899a6',
+  },
+};
