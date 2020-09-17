3d80e28e8dfd1078443c789b90647be8ede5a057
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-16 21:02:27 -0300

    add light theme

---
+++ src/styles/light.js
@@ -0,0 +1,13 @@
+export const LightTheme = {
+  dark: false,
+  colors: {
+    twitter: '#33a1f2',
+    primary: 'rgb(255, 45, 85)',
+    background: '#ffffff',
+    card: '#ffffff',
+    text: '#000000',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+    inactiveTintColor: '#8899a6',
+  },
+};
