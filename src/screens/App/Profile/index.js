ae56cb0b5d22bdbedf99b12892dfafd1f9044ca4
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-18 22:49:51 -0300

    profile screen

--- src/screens/App/Profile/index.js
+++ src/screens/App/Profile/index.js
@@ -1,12 +1,163 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 
-import {Container} from './styles';
+import ProfileTabs from '../../../components/ProfileTabs';
+import Tweet from '../../../components/Tweet';
 
-export default function Profile() {
+import balloon from '../../../assets/balloon.png';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  BackIcon,
+  MoreOptionsButton,
+  MoreOptionsIcon,
+  Avatar,
+  ProfileData,
+  EditProfileButtonWrapper,
+  EditProfileButton,
+  EditProfileText,
+  Name,
+  UserName,
+  City,
+  IconWrapper,
+  LocationIcon,
+  Birthday,
+  BalloonIcon,
+  SignUpDate,
+  CalendarIcon,
+  FollowInfo,
+  FollowingNumber,
+  FollowingText,
+  FollowersNumber,
+  FollowersText,
+  Tweets,
+} from './styles';
+
+export default function Profile({navigation}) {
+  const {colors} = useTheme();
+
   return (
     <Container>
-      <Text>Profile</Text>
+      <Header
+        source={{
+          uri:
+            'https://pbs.twimg.com/profile_banners/704295040982650880/1589387411/1500x500',
+        }}>
+        <BackButton onPress={() => navigation.goBack()}>
+          <BackIcon />
+        </BackButton>
+        <MoreOptionsButton>
+          <MoreOptionsIcon />
+        </MoreOptionsButton>
+        <Avatar
+          style={{borderColor: colors.card}}
+          source={{
+            uri:
+              'https://pbs.twimg.com/profile_images/1260607790323830791/NGrNpCkO_200x200.jpg',
+          }}
+        />
+      </Header>
+
+      <ProfileData>
+        <EditProfileButtonWrapper>
+          <EditProfileButton style={{borderColor: colors.gray}}>
+            <EditProfileText style={{color: colors.gray}}>
+              Editar perfil
+            </EditProfileText>
+          </EditProfileButton>
+        </EditProfileButtonWrapper>
+        <Name style={{color: colors.text}}>old wolf</Name>
+        <UserName style={{color: colors.gray}}>@filipem2210</UserName>
+        <City style={{color: colors.gray}}>
+          <IconWrapper>
+            <LocationIcon color={colors.gray} />
+          </IconWrapper>
+          Florianópolis
+        </City>
+        <Birthday style={{color: colors.gray}}>
+          <IconWrapper>
+            <BalloonIcon source={balloon} />
+          </IconWrapper>
+          Nascido em 22 de outubro de 1986
+        </Birthday>
+        <SignUpDate style={{color: colors.gray}}>
+          <IconWrapper>
+            <CalendarIcon color={colors.gray} />
+          </IconWrapper>
+          Entrou em fevereiro de 2016
+        </SignUpDate>
+        <FollowInfo>
+          <FollowingNumber style={{color: colors.text}}>78 </FollowingNumber>
+          <FollowingText style={{color: colors.gray}}>Seguindo </FollowingText>
+          <FollowersNumber style={{color: colors.text}}>22 </FollowersNumber>
+          <FollowersText style={{color: colors.gray}}>Seguidores</FollowersText>
+        </FollowInfo>
+      </ProfileData>
+      <ProfileTabs />
+      <Tweets style={{borderTopColor: colors.border}}>
+        <Tweet
+          avatar="https://pbs.twimg.com/profile_images/1299146910238945281/VckzvGa9_bigger.jpg"
+          name="Gabs Ferreira"
+          user="@o_gabsferreira"
+          date="20 de mai"
+          description="Entendendo a web em 4 passos simples"
+          image="https://pbs.twimg.com/media/EYfA145WAAE5Qm9?format=jpg&name=small"
+          comments={15}
+          retweets={281}
+          likes={125}
+          retweet
+        />
+        <Tweet
+          avatar="https://pbs.twimg.com/profile_images/1303700849559064585/F89znLqg_bigger.jpg"
+          name="talita souza"
+          user="@souzatali7"
+          date="10 de set"
+          description="queria ta tirando uma soneca com o ar condicionado no talo"
+          likes={1}
+        />
+        <Tweet
+          avatar="https://pbs.twimg.com/profile_images/1299146910238945281/VckzvGa9_bigger.jpg"
+          name="Gabs Ferreira"
+          user="@o_gabsferreira"
+          date="20 de mai"
+          description="Entendendo a web em 4 passos simples"
+          image="https://pbs.twimg.com/media/EYfA145WAAE5Qm9?format=jpg&name=small"
+          comments={15}
+          retweets={281}
+          likes={125}
+          retweet
+        />
+        <Tweet
+          avatar="https://pbs.twimg.com/profile_images/1303700849559064585/F89znLqg_bigger.jpg"
+          name="talita souza"
+          user="@souzatali7"
+          date="10 de set"
+          description="queria ta tirando uma soneca com o ar condicionado no talo"
+          likes={1}
+        />
+        <Tweet
+          avatar="https://pbs.twimg.com/profile_images/1299146910238945281/VckzvGa9_bigger.jpg"
+          name="Gabs Ferreira"
+          user="@o_gabsferreira"
+          date="20 de mai"
+          description="Entendendo a web em 4 passos simples"
+          image="https://pbs.twimg.com/media/EYfA145WAAE5Qm9?format=jpg&name=small"
+          comments={15}
+          retweets={281}
+          likes={125}
+          retweet
+        />
+        <Tweet
+          avatar="https://pbs.twimg.com/profile_images/1303700849559064585/F89znLqg_bigger.jpg"
+          name="talita souza"
+          user="@souzatali7"
+          date="10 de set"
+          description="queria ta tirando uma soneca com o ar condicionado no talo"
+          likes={1}
+        />
+      </Tweets>
     </Container>
   );
 }
--- src/screens/App/Profile/styles.js
+++ src/screens/App/Profile/styles.js
@@ -1,5 +1,151 @@
 import styled from 'styled-components/native';
+import ADIcon from 'react-native-vector-icons/AntDesign';
+import IoIcon from 'react-native-vector-icons/Ionicons';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
+`;
+
+export const Header = styled.ImageBackground`
+  position: relative;
+  width: 100%;
+  height: 150px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  padding: 5px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 30px;
+`;
+
+export const BackIcon = styled(ADIcon).attrs({
+  name: 'arrowleft',
+  size: 25,
+  color: '#fff',
+})``;
+
+export const MoreOptionsButton = styled.TouchableOpacity`
+  padding: 5px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 30px;
+`;
+
+export const MoreOptionsIcon = styled(IoIcon).attrs({
+  name: 'ellipsis-vertical-outline',
+  size: 25,
+  color: '#fff',
+})``;
+
+export const Avatar = styled.Image`
+  position: absolute;
+  bottom: -40px;
+  left: 15px;
+  width: 75px;
+  height: 75px;
+  border-bottom-left-radius: 75px;
+  border-bottom-right-radius: 75px;
+  border-top-right-radius: 75px;
+  border-top-left-radius: 75px;
+  border-width: 4px;
+`;
+
+export const ProfileData = styled.View`
+  padding: 8px 15px;
+`;
+
+export const EditProfileButtonWrapper = styled.View`
+  width: 100%;
+  align-items: flex-end;
+`;
+
+export const EditProfileButton = styled.TouchableOpacity`
+  padding: 6px 20px;
+  border-width: 1px;
+  border-color: gray;
+  border-radius: 20px;
+  background-color: transparent;
+`;
+
+export const EditProfileText = styled.Text`
+  font-weight: bold;
+`;
+
+export const Name = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const UserName = styled.Text`
+  font-size: 14px;
+`;
+
+export const IconWrapper = styled.View`
+  width: 20px;
+  margin-right: 5px;
+`;
+
+export const City = styled.Text`
+  font-size: 14px;
+  margin-top: 20px;
+  align-items: center;
+`;
+
+export const LocationIcon = styled(IoIcon).attrs({
+  name: 'location-outline',
+  size: 15,
+})`
+  width: 20px;
+`;
+
+export const Birthday = styled.Text`
+  font-size: 14px;
+  margin-top: 3px;
+  align-items: center;
+`;
+
+export const BalloonIcon = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
+  width: 12px;
+  height: 12px;
+`;
+
+export const SignUpDate = styled.Text`
+  font-size: 14px;
+  margin-top: 3px;
+  align-items: center;
+`;
+
+export const CalendarIcon = styled(IoIcon).attrs({
+  name: 'calendar-outline',
+  size: 15,
+})`
+  width: 20px;
+`;
+
+export const FollowInfo = styled.View`
+  flex-direction: row;
+  margin-top: 10px;
+`;
+
+export const FollowingNumber = styled.Text`
+  font-weight: bold;
+`;
+
+export const FollowingText = styled.Text``;
+
+export const FollowersNumber = styled.Text`
+  font-weight: bold;
+`;
+
+export const FollowersText = styled.Text``;
+
+export const Tweets = styled.View`
+  flex: 1;
+  border-top-width: 1px;
 `;
