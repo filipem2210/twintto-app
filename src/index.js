975e07c28a8aa6e8e939ea58328515834734a613
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-23 20:54:25 -0300

    add theme provider to its own file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
-import React, {useState, useMemo} from 'react';
-import {StatusBar} from 'react-native';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {DarkTheme} from './styles/dark';
-import {LightTheme} from './styles/light';
-
-import {ThemeContext} from './contexts/ThemeContext';
+import ThemeContextProvider from './utils/toggleTheme';
 
 import Routes from './routes';
 
 export default function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-  const theme = isDarkTheme ? DarkTheme : LightTheme;
-
-  const themeContext = useMemo(
-    () => ({
-      toggleTheme: () => {
-        setIsDarkTheme((prevValue) => !prevValue);
-      },
-    }),
-    [],
-  );
-
   return (
-    <NavigationContainer theme={theme}>
-      <ThemeContext.Provider value={themeContext}>
-        <StatusBar barStyle={isDarkTheme ? 'dark-content' : 'light-content'} />
+    <NavigationContainer>
+      <ThemeContextProvider>
         <Routes />
-      </ThemeContext.Provider>
+      </ThemeContextProvider>
     </NavigationContainer>
   );
 }
---
+++ src/utils/toggleTheme.js
@@ -0,0 +1,32 @@
+import React, {useState, useMemo} from 'react';
+import {StatusBar} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+
+import {DarkTheme} from '../styles/dark';
+import {LightTheme} from '../styles/light';
+
+import {ThemeContext} from '../contexts/ThemeContext';
+
+export default function ToggleTheme({children}) {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const theme = isDarkTheme ? DarkTheme : LightTheme;
+
+  const themeContext = useMemo(
+    () => ({
+      toggleTheme: () => {
+        setIsDarkTheme((prevValue) => !prevValue);
+      },
+    }),
+    [],
+  );
+
+  return (
+    <ThemeContext.Provider value={themeContext}>
+      <ThemeProvider theme={theme}>
+        <StatusBar barStyle={isDarkTheme ? 'dark-content' : 'light-content'} />
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+}
